90352de25fa98edd756c569e532f23ca0ed0053c
alexherns <alexherns@gmail.com>
2015-09-15 21:13:06 -0700

    Untracked the flask files

--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ from flask import render_template, flash, redirect, session, url_for, request
 from .forms import searchBox
 from .models import Article
 from .scripts import fetch_articles, query_local_database
-import json, urllib2
+import json, urllib2, pickle
 from config import ALTMETRIC_KEY
 
 @app.route('/', methods=['GET', 'POST'])
@@ -24,8 +24,7 @@ def results():
     if default_option == 'True':
         articles= models.Article.query.filter(models.Article.default_set == True).order_by(models.Article.score.desc()).all()
     else:
-        #articles= models.Article.query.filter(models.Article.default_set == False).order_by(models.Article.score.desc()).all()
-        articles= request.args['articles']
+        articles= pickle.load(open('./tmp/pickle.dump', 'rb'))
     if form.validate_on_submit():
             flash('Search for {0} was successfully accepted!'.format(form.query_term.data))
             return redirect(url_for('search', query_term=form.query_term.data))
@@ -34,25 +33,11 @@ def results():
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     query_term= request.args['query_term']
-    """try:
-        response= urllib2.urlopen('http://api.altmetric.com/v1/citations/1w?q={0}&key={1}'.format(query_term, ALTMETRIC_KEY))
-    except urllib2.HTTPError:
-        flash('No results were obtained for your query. Returning a default set.'.format(query_term))
-        default_option= True
-        return redirect(url_for('results', default_option=default_option))
-    default_option= False
-    data= json.load(response)
-    articles= fetch_articles(data)
-    form= searchBox()
-    old_articles= models.Article.query.filter(models.Article.default_set == False).all()
-    for a in old_articles:
-        db.session.delete(a)
-    for article in articles:
-        db.session.add(article)
-    db.session.commit()"""
-    articles= query_local_database(query)
+    articles= query_local_database(query_term)
     if len(articles) == 0:
         flash('No results were obtained for your query. Returning a default set.'.format(query_term))
         default_option= True
         return redirect(url_for('results', default_option=default_option))
-    return redirect(url_for('results', default_option=default_option, articles=articles))
+    default_option= False
+    pickle.dump(articles, open('./tmp/pickle.dump', 'wb'))
+    return redirect(url_for('results', default_option=default_option))
